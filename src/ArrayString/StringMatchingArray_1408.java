de614b78d3541ed727fdfd520928cb9be37ee0b6
Payal Gaikwad <payalgaikwad21@gmail.com>
2023-09-30 15:09:25 +0530

    changes on Array and Employee class

--- src/ArrayString/StringMatchingArray_1408.java
+++ src/ArrayString/StringMatchingArray_1408.java
@@ -4,7 +4,9 @@ public class StringMatchingArray_1408 {
 
 	static public void main(String[] args) {
 		
-		System.out.println("Git new branch test");
+		System.out.println("Git Clone and make changes");
+		System.out.println("Git push and merge in master");
+
 	}
 	
 }
--- src/EasyPrograms/Employee.java
+++ src/EasyPrograms/Employee.java
@@ -1,14 +1,17 @@
 package EasyPrograms;
 
+import org.testng.annotations.Test;
+
 public class Employee {
 	
 	 String firstName;
      String lastName;
     
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Test
+	public void EmployeeTest() {
+		System.out.println("Employee is added.!");
+		
 	}
 
 }
